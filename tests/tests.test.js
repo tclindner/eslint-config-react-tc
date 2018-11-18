c5985a1023c781b9eaaacac88427337d93538ab5
tclindner <tclindner@users.noreply.github.com>
2018-11-17 19:20:45 -0600

    Add log

--- tests/tests.test.js
+++ tests/tests.test.js
@@ -38,6 +38,8 @@ describe('eslint config tests', () => {
       const errors = lint(code, eslintConfig);
       const error = errors[0];
 
+      console.log(errors);
+
       error.ruleId.should.equal('react/react-in-jsx-scope');
       error.line.should.equal(expectedErrorLineNum);
       error.column.should.equal(expectedErrorColumnNum);
